61a2b2935ba4bfbdc22116be6108da6676dc5974
HomeroBR26 <homero.hbr@gmail.com>
2015-12-12 16:20:47 -0600

    http_request.py added.

---
+++ http_request.py
@@ -0,0 +1,26 @@
+"""HTTP Requests with Python"""
+
+import socket
+
+def make_socket():
+    """Create a socket to a host, port."""
+
+    my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    my_socket.connect(('www.py4inf.com', 80))
+    my_socket.send('GET http://www.py4inf.com/code/romeo.txt HTTP/1.0\n\n')
+    return my_socket
+
+def main():
+    """Main function."""
+
+    my_socket = make_socket()
+    print "\n"
+    while True:
+        data = my_socket.recv(512)
+        if len(data) < 1:
+            break
+        print data
+    my_socket.close()
+
+if __name__ == '__main__':
+    main()
